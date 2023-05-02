a4c38159653a1c71b08c174f66d01a342ceb5b85
Auxilor <william.favierparsons1@gmail.com>
2023-05-02 15:50:58 +0100

    Updated to use PlaceholderContext

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecobits/currencies/PriceFactoryCurrency.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecobits/currencies/PriceFactoryCurrency.kt
@@ -1,6 +1,8 @@
 package com.willfp.ecobits.currencies
 
 import com.willfp.eco.core.math.MathContext
+import com.willfp.eco.core.placeholder.context.PlaceholderContext
+import com.willfp.eco.core.placeholder.context.PlaceholderContextSupplier
 import com.willfp.eco.core.price.Price
 import com.willfp.eco.core.price.PriceFactory
 import org.bukkit.entity.Player
@@ -14,14 +16,14 @@ class PriceFactoryCurrency(
         currency.id
     )
 
-    override fun create(baseContext: MathContext, function: Function<MathContext, Double>): Price {
-        return PriceCurrency(currency, baseContext) { function.apply(it) }
+    override fun create(baseContext: PlaceholderContext, function: PlaceholderContextSupplier<Double>): Price {
+        return PriceCurrency(currency, baseContext) { function.get(it) }
     }
 
     private class PriceCurrency(
         private val currency: Currency,
-        private val baseContext: MathContext,
-        private val xp: (MathContext) -> Double
+        private val baseContext: PlaceholderContext,
+        private val xp: (PlaceholderContext) -> Double
     ) : Price {
         private val multipliers = mutableMapOf<UUID, Double>()
 
@@ -35,7 +37,7 @@ class PriceFactoryCurrency(
             player.adjustBalance(currency, getValue(player, multiplier))
 
         override fun getValue(player: Player, multiplier: Double) =
-            xp(MathContext.copyWithPlayer(baseContext, player)) * getMultiplier(player) * multiplier
+            xp(baseContext.copyWithPlayer(player)) * getMultiplier(player) * multiplier
 
         override fun getMultiplier(player: Player): Double {
             return multipliers[player.uniqueId] ?: 1.0

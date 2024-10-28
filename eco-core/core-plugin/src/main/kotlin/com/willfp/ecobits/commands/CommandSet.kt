af5be65a896625188b4e5ee582c27d8ca4de181e
666pyke <140652366+666pyke@users.noreply.github.com>
2024-10-28 20:09:41 +0200

    Update CommandSet.kt

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecobits/commands/CommandSet.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecobits/commands/CommandSet.kt
@@ -5,22 +5,23 @@ import com.willfp.eco.core.command.impl.Subcommand
 import com.willfp.eco.util.StringUtils
 import com.willfp.eco.util.savedDisplayName
 import com.willfp.eco.util.toNiceString
+import com.willfp.ecobits.EcoBitsPlugin
 import com.willfp.ecobits.currencies.Currencies
 import com.willfp.ecobits.currencies.Currency
-import com.willfp.ecobits.currencies.adjustBalance
 import com.willfp.ecobits.currencies.setBalance
 import org.bukkit.Bukkit
+import org.bukkit.OfflinePlayer
 import org.bukkit.command.CommandSender
 import org.bukkit.util.StringUtil
 
 class CommandSet(
-    plugin: EcoPlugin,
-    private val currency: Currency? = null
+        plugin: EcoPlugin,
+        private val currency: Currency? = null
 ) : Subcommand(
-    plugin,
-    "set",
-    "ecobits.command.set",
-    false
+        plugin,
+        "set",
+        "ecobits.command.set",
+        false
 ) {
     private val argOffset = if (currency == null) 0 else -1
 
@@ -30,10 +31,21 @@ class CommandSet(
             return
         }
 
-        @Suppress("DEPRECATION")
-        val player = Bukkit.getOfflinePlayer(args[0])
+        val allowOfflinePlayers = EcoBitsPlugin.instance.allowOfflinePlayers
+        val targetPlayer: OfflinePlayer? = if (allowOfflinePlayers) {
+            Bukkit.getOfflinePlayer(args[0])
+        } else {
+            Bukkit.getPlayer(args[0])
+        }
 
-        if (!player.hasPlayedBefore() && !player.isOnline) {
+        // Don't allow the usage on offline players
+        if (targetPlayer == null || (!allowOfflinePlayers && !targetPlayer.isOnline)) {
+            sender.sendMessage(plugin.langYml.getMessage("player-not-online"))
+            return
+        }
+
+        // Don't allow the usage on player that don't exist
+        if (!targetPlayer.hasPlayedBefore() && !targetPlayer.isOnline) {
             sender.sendMessage(plugin.langYml.getMessage("invalid-player"))
             return
         }
@@ -64,13 +76,13 @@ class CommandSet(
             return
         }
 
-        player.setBalance(currency, amount.toBigDecimal())
+        targetPlayer.setBalance(currency, amount.toBigDecimal())
 
         sender.sendMessage(
-            plugin.langYml.getMessage("set-currency", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
-                .replace("%player%", player.savedDisplayName)
-                .replace("%amount%", amount.toNiceString())
-                .replace("%currency%", currency.name)
+                plugin.langYml.getMessage("set-currency", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
+                        .replace("%player%", targetPlayer.savedDisplayName)
+                        .replace("%amount%", amount.toNiceString())
+                        .replace("%currency%", currency.name)
         )
     }
 
@@ -78,32 +90,38 @@ class CommandSet(
         val completions = mutableListOf<String>()
 
         if (args.isEmpty()) {
-            return Bukkit.getOnlinePlayers().map { it.name }
+            return if (EcoBitsPlugin.instance.allowOfflinePlayers) {
+                Bukkit.getOfflinePlayers().mapNotNull { it.name }
+            } else {
+                Bukkit.getOnlinePlayers().map { it.name }
+            }
         }
 
         if (args.size == 1) {
             StringUtil.copyPartialMatches(
-                args[0],
-                Bukkit.getOnlinePlayers().map { it.name },
-                completions
+                    args[0],
+                    if (EcoBitsPlugin.instance.allowOfflinePlayers) {
+                        Bukkit.getOfflinePlayers().map { it.name }
+                    } else {
+                        Bukkit.getOnlinePlayers().map { it.name }
+                    },
+                    completions
             )
         }
 
-        if (this.currency == null) {
-            if (args.size == 2) {
-                StringUtil.copyPartialMatches(
+        if (this.currency == null && args.size == 2) {
+            StringUtil.copyPartialMatches(
                     args[1],
                     Currencies.values().map { it.id },
                     completions
-                )
-            }
+            )
         }
 
         if (args.size == 3 + argOffset) {
             StringUtil.copyPartialMatches(
-                args[2 + argOffset],
-                arrayOf(1, 2, 3, 4, 5).map { it.toString() },
-                completions
+                    args[2 + argOffset],
+                    arrayOf(1, 2, 3, 4, 5).map { it.toString() },
+                    completions
             )
         }
 

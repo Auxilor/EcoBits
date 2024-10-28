f4378090a5133e8076f59fe03fbb581bbbc803eb
666pyke <140652366+666pyke@users.noreply.github.com>
2024-10-28 20:08:36 +0200

    Update CommandGet.kt

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecobits/commands/CommandGet.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecobits/commands/CommandGet.kt
@@ -5,21 +5,23 @@ import com.willfp.eco.core.command.impl.Subcommand
 import com.willfp.eco.util.StringUtils
 import com.willfp.eco.util.savedDisplayName
 import com.willfp.eco.util.toNiceString
+import com.willfp.ecobits.EcoBitsPlugin
 import com.willfp.ecobits.currencies.Currencies
 import com.willfp.ecobits.currencies.Currency
 import com.willfp.ecobits.currencies.getBalance
 import org.bukkit.Bukkit
+import org.bukkit.OfflinePlayer
 import org.bukkit.command.CommandSender
 import org.bukkit.util.StringUtil
 
 class CommandGet(
-    plugin: EcoPlugin,
-    private val currency: Currency? = null
+        plugin: EcoPlugin,
+        private val currency: Currency? = null
 ) : Subcommand(
-    plugin,
-    "get",
-    "ecobits.command.get",
-    false
+        plugin,
+        "get",
+        "ecobits.command.get",
+        false
 ) {
     override fun onExecute(sender: CommandSender, args: List<String>) {
         if (args.isEmpty()) {
@@ -27,10 +29,21 @@ class CommandGet(
             return
         }
 
-        @Suppress("DEPRECATION")
-        val player = Bukkit.getOfflinePlayer(args[0])
+        val allowOfflinePlayers = EcoBitsPlugin.instance.allowOfflinePlayers
+        val targetPlayer: OfflinePlayer? = if (allowOfflinePlayers) {
+            Bukkit.getOfflinePlayer(args[0])
+        } else {
+            Bukkit.getPlayer(args[0])
+        }
 
-        if (!player.hasPlayedBefore() && !player.isOnline) {
+        // Don't allow the usage on offline players
+        if (targetPlayer == null || (!allowOfflinePlayers && !targetPlayer.isOnline)) {
+            sender.sendMessage(plugin.langYml.getMessage("player-not-online"))
+            return
+        }
+
+        // Don't allow the usage on player that don't exist
+        if (!targetPlayer.hasPlayedBefore() && !targetPlayer.isOnline) {
             sender.sendMessage(plugin.langYml.getMessage("invalid-player"))
             return
         }
@@ -50,10 +63,10 @@ class CommandGet(
         }
 
         sender.sendMessage(
-            plugin.langYml.getMessage("other-balance", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
-                .replace("%player%", player.savedDisplayName)
-                .replace("%amount%", player.getBalance(currency).toNiceString())
-                .replace("%currency%", currency.name)
+                plugin.langYml.getMessage("other-balance", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
+                        .replace("%player%", targetPlayer.savedDisplayName)
+                        .replace("%amount%", targetPlayer.getBalance(currency).toNiceString())
+                        .replace("%currency%", currency.name)
         )
     }
 
@@ -66,20 +79,18 @@ class CommandGet(
 
         if (args.size == 1) {
             StringUtil.copyPartialMatches(
-                args[0],
-                Bukkit.getOnlinePlayers().map { it.name },
-                completions
+                    args[0],
+                    Bukkit.getOnlinePlayers().map { it.name },
+                    completions
             )
         }
 
-        if (this.currency == null) {
-            if (args.size == 2) {
-                StringUtil.copyPartialMatches(
+        if (this.currency == null && args.size == 2) {
+            StringUtil.copyPartialMatches(
                     args[1],
                     Currencies.values().map { it.id },
                     completions
-                )
-            }
+            )
         }
 
         return completions

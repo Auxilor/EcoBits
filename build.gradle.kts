648969291f8e1f2634d7adb42d3ee5dd2acfeb06
NicoNeko <nicuch100@gmail.com>
2026-03-07 23:11:07 +0200

    Make it a eco plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,10 @@ plugins {
     id("java-library")
     id("maven-publish")
     id("com.gradleup.shadow") version "9.3.1"
-    id("com.willfp.libreforge-gradle-plugin") version "1.0.3"
 }
 
 group = "com.willfp"
 version = findProperty("version")!!
-val libreforgeVersion = findProperty("libreforge-version")
 
 base {
     archivesName.set(project.name)
@@ -41,7 +39,7 @@ allprojects {
     }
 
     dependencies {
-        compileOnly("com.willfp:eco:6.77.3")
+        compileOnly("com.willfp:eco:6.77.6")
         compileOnly("org.jetbrains:annotations:26.0.2")
         compileOnly("org.jetbrains.kotlin:kotlin-stdlib:2.3.0")
         compileOnly("com.github.ben-manes.caffeine:caffeine:3.2.3")
@@ -55,10 +53,6 @@ allprojects {
     }
 
     tasks {
-        shadowJar {
-            relocate("com.willfp.libreforge.loader", "com.willfp.ecoitems.libreforge.loader")
-        }
-
         compileKotlin {
             compilerOptions {
                 jvmTarget.set(JvmTarget.JVM_21)
@@ -73,10 +67,9 @@ allprojects {
         }
 
         processResources {
-            filesMatching(listOf("**plugin.yml", "**eco.yml")) {
+            filesMatching(listOf("**plugin.yml")) {
                 expand(
                     "version" to project.version,
-                    "libreforgeVersion" to libreforgeVersion!!,
                     "pluginName" to rootProject.name
                 )
             }
@@ -85,5 +78,33 @@ allprojects {
         build {
             dependsOn(shadowJar)
         }
+    }
+}
+
+tasks {
+    build {
+        dependsOn("uberJar")
+    }
+
+    register("uberJar", Jar::class.java) {
+        destinationDirectory.set(file("$rootDir/bin"))
+        archiveFileName.set("${project.name} v${project.version}.jar")
+
+        dependsOn(getByName("shadowJar"))
+
+        from(
+            getByName("shadowJar").outputs.files.map { file -> zipTree(file) }
+
+        )
+    }
+
+    register("cleanJar") {
+        doLast {
+            file("$rootDir/bin").deleteRecursively()
+        }
+    }
+
+    clean {
+        dependsOn(getByName("cleanJar"))
     }
 }

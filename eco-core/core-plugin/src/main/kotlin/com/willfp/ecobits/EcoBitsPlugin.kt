c192427008fc62d857c4dfdbf6cba87f3f584b3d
Callum Johnson <74352969+CallumJohnson@users.noreply.github.com>
2026-05-16 15:37:05 +0100

    feat: BStats Metrics

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecobits/EcoBitsPlugin.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecobits/EcoBitsPlugin.kt
@@ -1,6 +1,7 @@
 package com.willfp.ecobits
 
 import com.willfp.eco.core.EcoPlugin
+import com.willfp.eco.core.bstats.EcoMetricsChart
 import com.willfp.eco.core.command.impl.PluginCommand
 import com.willfp.eco.util.ClassUtils
 import com.willfp.ecobits.commands.CommandEcoBits
@@ -42,5 +43,17 @@ class EcoBitsPlugin : EcoPlugin() {
             CommandEcoBits
         )
     }
+
+    override fun getCustomCharts() = listOf(
+        EcoMetricsChart.SingleLine("total_currencies") { Currencies.values().size },
+        EcoMetricsChart.SingleLine("vault_currencies") { Currencies.values().count { it.isRegisteredWithVault } },
+        EcoMetricsChart.SingleLine("payable_currencies") { Currencies.values().count { it.isPayable } },
+        EcoMetricsChart.SimplePie("leaderboard_enabled") {
+            if (configYml.getBool("leaderboard.enabled")) "enabled" else "disabled"
+        },
+        EcoMetricsChart.SimplePie("vault_present") {
+            if (IntegrationVault.isVaultPresent) "present" else "absent"
+        }
+    )
 }
 
